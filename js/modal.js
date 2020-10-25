f5bd69290ae2e59d7359e7a05278ee30c70ef40f
Skip Khamvongsa <skipk95@gmail.com>
2020-10-25 13:54:59 -0600

    Added modal js file, and tested onclick function

---
+++ js/modal.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    const modalText = {
+        trailsOnTap: {
+            title: 'Trails on Tap',
+            tag: 'Hiking trails & Breweries',
+            detail: 'Trails on Tap is a application for users to simply plan a full adventure with a search of city or zip, to find hiking trails, and breweries. Be outdoors with friends, and receive a reward after your hard work.',
+            link: 'https://github.com/skip1113/class-pro-1',
+            demo: 'https://skip1113.github.io/class-pro-1/views/index.html'
+        }
+    }
+
+    $('#gallery .button').on('click', function () {
+        alert("you've clicked");
+    });
+});
+
